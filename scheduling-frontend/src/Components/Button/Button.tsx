e3b7d73e8dc281b3010669f35c31da05f65a3b20
MrV09 <vladcirlugea@gmail.com>
2025-05-24 13:26:18 +0300

    OPS-17 Created basic landing page

---
+++ scheduling-frontend/src/Components/Button/Button.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import classNames from 'classnames';
+import './Button.css'
+
+interface ButtonProps {
+    children: React.ReactNode;
+    onClick: () => void;
+    type?: 'button' | 'submit' | 'reset';
+    variant?: 'primary' | 'secondary' | 'danger';
+    disabled?: boolean;
+    className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    children,
+    onClick,
+    type = 'button',
+    variant = 'primary',
+    disabled = false,
+    className = '',
+}) => {
+  return (
+    <button type={type} onClick={onClick} disabled={disabled} className={classNames("button", variant, className)} >
+        {children}
+    </button>
+  )
+}
+
+export default Button
